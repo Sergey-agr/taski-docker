537db0af648e387ee4410ebd53815fd962f764ee
Сергей Баймулдинов <makemealiveAwesome@yandex.ru>
2024-07-07 15:20:25 +0300

    ffs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.70.165', '127.0.0.1', 'localhost', 'practicumstudy.ddns.net']
-
-
 # Application definition
 
 INSTALLED_APPS = [
